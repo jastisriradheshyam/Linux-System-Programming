f514c87dfb65c1b480b0f418a3c641d9f19cbac7
Jasti Sri Radhe Shyam <jastisriradheshyam@users.noreply.github.com>
2017-10-08 15:39:30 +0530

    Update copy.c
    Added more comments

--- copy.c
+++ copy.c
@@ -1,9 +1,9 @@
 /* Trivial file copy program using low-level I/O */
 
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
+#include <fcntl.h> /* for open and it's flags like O_RDONLY, etc. */
+#include <stdlib.h> /* for exit */
+#include <unistd.h> /* for read, write, close*/
+#include <stdio.h> /* for perror */
 #define BSIZE 16384  //buffer size
 
 void main()
@@ -12,16 +12,16 @@ void main()
  char buf[BSIZE];
  int count;
  
- if ((fin = open("foo", O_RDONLY)) < 0) {
-  perror("foo");
+ if ((fin = open("foo", O_RDONLY)) < 0) { /* Checks if file exists and if exists then open the file */
+  perror("foo"); /* Shows before error is printed */
   exit(1);
  }
  if ((fout = open("bar", O_WRONLY | O_CREAT, 0644)) < 0) {
-  perror("bar");
+  perror("bar"); /* Shows before error is printed */
   exit(2);
  }
  while ((count = read(fin, buf, BSIZE)) > 0)
-  write(fout, buf, count);
+  write(fout, buf, count); /* write to the file */
  
  close(fin);
  close(fout);
