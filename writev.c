bfcfa325ee25abd0d3d35230568924aaaeac963d
Jasti Sri Radhe Shyam <jastisriradheshyam@users.noreply.github.com>
2017-10-18 08:17:45 +0530

    Update writev.c
    Added comments

--- writev.c
+++ writev.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
-#include <sys/types.h>
-//#include <sys/stat.h>
-#include <fcntl.h>
-#include <string.h>
-#include <sys/uio.h>
-#include <unistd.h>
+#include <stdio.h> /* perror, printf function */
+#include <sys/types.h> /* ssize */
+#include <fcntl.h> /* open function and flags (O_CREAT,etc) */
+#include <string.h> /* strlen function */
+#include <sys/uio.h> /* iovec structure, writev function */
+#include <unistd.h> /* close function */
 int main ()
 {
- struct iovec iov[3];
- ssize_t no_of_bytes;
+ struct iovec iov[3]; /* created iovec array object */
+ ssize_t no_of_bytes; /* bytes variable for error */
  int fd, i;
  char *buf[] = {"This is an example of writev function.\n","This function is specified in open group.\n","This function is used in Linux and Unix systems.\n"};
- fd = open ("writev_eg.txt", O_WRONLY | O_CREAT | O_TRUNC);
- if(fd == -1){
+ fd = open ("writev_eg.txt", O_WRONLY | O_CREAT | O_TRUNC); /*open writev_eg.txt under write, create if not created, truncate options */
+ if(fd == -1){ /* file error checking */
   perror ("open");
   return 1;
  }
@@ -22,14 +21,14 @@ int main ()
   iov[i].iov_len = strlen(buf[i]);
  }
  /* with a single call, write them all out */
- no_of_bytes = writev (fd, iov, 3);
+ no_of_bytes = writev (fd, iov, 3); /* write the data 
  if(no_of_bytes == -1){
   perror ("writev");
   return 1;
  }
  printf ("wrote %ld bytes\n", no_of_bytes);
 
- if(close (fd)) {
+ if(close (fd)) { /* close file */
   perror ("close");
   return 1;
  }
